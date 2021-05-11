435b9d85606be6f94e6425167a42f9a17f57b3a9
Soo-Beom <soobeomkim77@gmail.com>
2021-05-11 16:17:20 -0700

    minor adjustments to comment request

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,13 @@
 class CommentsController < ApplicationController
     def create
-        @comment = Comment.create(comment_params)
+        byebug
+        @comment = Comment.new(comment_params)
+        @comment.user = @@user
+        @comment.save
         if @comment.valid?
             render json: @comment, status: :accepted
         else
-            render json: {message: "You fucked up.", errors: @blog.errors}, status: :not_acceptable
+            render json: {message: "You fucked up.", errors: @comment.errors}, status: :not_acceptable
         end
     end
 
@@ -14,6 +17,6 @@ class CommentsController < ApplicationController
 
     private 
         def comment_params
-            params.require(:comment).permit(:content, :user_id, :blog_id)
+            params.require(:comment).permit(:content, :blog_id)
         end
 end
